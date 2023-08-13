f14ff0c25934f2415b3896acda7a9eb77f21e336
kiwon <kkw-11@naver.com>
2023-08-13 14:19:39 +0900

    feat:AppConfig설정

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,13 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     //순수 자바로 애플리케이션 로직 개발
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = new MemberServiceImpl();
+        MemberService memberService = appConfig.memberService();
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -7,7 +7,12 @@ public class MemberServiceImpl implements MemberService{
      MemberRepository 라는 역할에 의존하고 있지만 추가로 MemoryMemberRepository 라는 구체화에도 의존 -> DIP 의존관계 역전원칙 위배
      게다가, MemoryMemberRepository는 나중에 DB가 확정 되면 변경해야하는데 이때 OCP 원칙 위배
     */
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
